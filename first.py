eeb523b43f8cf7ed0ce3591406c80e231c51db43
Sudhanshu Shekhar <sudhanshu2407@gmail.com>
2025-01-12 20:32:29 +0530

    first Commit

---
+++ first.py
@@ -0,0 +1,9 @@
+# print("Hello World")
+a=5
+print(5)
+
+instructor='Sudhanshu SHekhar'
+
+print("python by ",instructor, sep='-')
+
+
---
+++ variables.py
@@ -0,0 +1,5 @@
+MyName='Sudhanshu'  #pascal case
+myName='Sudhanshu'  #camel case
+myname='Sudhanshu'  #flat case
+my_name='Sudhanshu' #snake case
+
